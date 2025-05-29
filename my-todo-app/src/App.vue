7e2c5d12aa08c433c8521e4a72f6e805ab407f2f
LorenzoFormatemp <lorenzotesoro20@gmail.com>
2025-05-29 16:49:50 +0200

    begin taskList component

--- my-todo-app/src/App.vue
+++ my-todo-app/src/App.vue
@@ -6,15 +6,17 @@
     <AddTask @add-task="addTask"/>
     
     <!-- componente: lista dei task -->
+    <TaskList />
 
   </div>  
 </template>
 
 <script>
 import AddTask from './components/AddTask.vue'
+import TaskList from './components/TaskList.vue'
 
 export default {
-  components: { AddTask },
+  components: { AddTask, TaskList },
   data() {
     return {
       // variabili
---
+++ my-todo-app/src/components/TaskList.vue
@@ -0,0 +1,14 @@
+<template>
+    <!-- ciclo di tutti i task -->
+    <!-- se l'array dei task ne ha almeno 1 allora ciclami l'array -->
+    <!-- altrimenti mostra un messaggio tipo; 'aggiungi dei task' -->
+    <p>No tasks. Add at least one task</p>
+</template>
+
+<script>
+export default {    
+}
+
+</script>
+
+<style scoped></style>
