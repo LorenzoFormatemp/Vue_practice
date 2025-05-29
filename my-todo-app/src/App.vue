6fb425b92b9efab996d5baf2921326e507e9e58a
LorenzoFormatemp <lorenzotesoro20@gmail.com>
2025-05-29 15:59:04 +0200

    add AddTask component

--- my-todo-app/src/App.vue
+++ my-todo-app/src/App.vue
@@ -1,17 +1,30 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AddTask from './components/AddTask.vue'
+
+export default {
+  components: { AddTask },
+  data() {
+    return {
+      // variabili
+    }
+  },
+  methods: {
+    // metodi/funzioni
+  }
+
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="container">
+    <h1>To Do App con Vue.Js</h1>
+
+    <!-- componente: aggiunta dei task -->
+    <AddTask />
+    
+    <!-- componente: lista dei task -->
+
+  </div>  
 </template>
 
 <style scoped>
---
+++ my-todo-app/src/components/AddTask.vue
@@ -0,0 +1,26 @@
+<template>
+    <!-- html -->
+    <form action="">
+        <input type="text" class="form-control" placeholder="Enter your task" required>
+    </form>
+
+
+</template>
+
+<script>
+/* js */
+export default {
+    data() {
+        return {
+            // variabili
+        }
+    },
+    methods: {
+        // funzioni
+    }
+}
+
+
+</script>
+
+<style scoped></style>
